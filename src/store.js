2816510aadc21b5786aee0be707bb6c5db570531
impiia <impiia28@gmail.com>
2023-10-22 00:08:02 +0400

    Add files via upload

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-// импортируем функцию createStore из Redux
-import { createStore } from "redux";
+// импортируем функцию configureStore из @reduxjs/toolkit
+import { configureStore } from "@reduxjs/toolkit";
 
 // импортируем наш редьюсер
 import reducer from "./reducer";
 
-// создаем хранилище с помощью редьюсера
-const store = createStore(reducer);
+// создаем хранилище с помощью функции configureStore и передаем ей наш редьюсер
+const store = configureStore({ reducer });
 
 // экспортируем хранилище
 export default store;
